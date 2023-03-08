4fe38424d80c0230c9d08bd9c4fe0bf316fbc7e7
Rogerio Ribeiro <rogerioribeiro@rdtechfitness.com.br>
2023-03-08 10:52:15 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,7 +24,15 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-
+	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+		
+		
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -55,6 +63,7 @@ public class ChessMatch {
 			throw new ChessException("The chosen piece can't move the target position");
 		}
 	}
+	
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
